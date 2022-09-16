d3fa3e9b0318b6293ea36db8dda56700da526ea1
= <=>
2022-09-16 09:05:29 +0600

    Variable Naming convention

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+
+/*
+* Variable Naming Allowed
+* camelCase // userName
+* Only letters and digits // ageGroup5
+* Starting with $ is allowed // $kindOFSpecial
+* Starting with _ is allowed // _internalValue
+* */
+let currentResult =
