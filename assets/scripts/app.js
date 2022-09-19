bc8c0787b6df18cd2551799a0d5da07418ddd952
= <=>
2022-09-19 18:41:30 +0600

    Create variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,3 @@
-
 /*
 * Variable Naming Allowed
 * camelCase // userName
@@ -6,4 +5,20 @@
 * Starting with $ is allowed // $kindOFSpecial
 * Starting with _ is allowed // _internalValue
 * */
-let currentResult =
+
+/*
+* Variable Naming Not Allowed / Not Recommended
+* let user_name // Allowed but bad practice!
+* let 21Players // Starting digits not allowed
+* let user-b // No special characters!
+* let let // Keywords not allowed
+* */
+
+let userName = 'Navil Sarker';
+const userAge = 27;
+let userJob = 'Software Engineer';
+
+
+console.log(userName);
+console.log(userAge)
+console.log(userJob)
