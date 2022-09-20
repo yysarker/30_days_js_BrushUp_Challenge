7d6f4a9a8122b5fd1fa712d08bd93e0bd343c26c
= <=>
2022-09-20 17:02:07 +0600

    correct variable name

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const defaultNumber = 0;
-let currentNumber = defaultNumber;
+let currentResult = defaultNumber;
 let logEntries=[];
 
 // Gets input from Input field
@@ -10,7 +10,7 @@ function getUserInputNumber() {
 // Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `(${resultBeforeCalc} ${operator} ${calcNumber})`;
-    outputResult(currentNumber, calcDescription);//came from vendor file
+    outputResult(currentResult, calcDescription);//came from vendor file
 }
 
 function writeToLog(operationIdentifier, prevResult, operationNumber, newNumber) {
@@ -27,37 +27,37 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newNumber)
 // This function work for Addition
 function add() {
     const enterNumber = getUserInputNumber();
-    const initialNumber = currentNumber;
-    currentNumber += enterNumber; // Full form of this code is currentResult = currentResult + enterNumber
+    const initialNumber = currentResult;
+    currentResult += enterNumber; // Full form of this code is currentResult = currentResult + enterNumber
     createAndWriteOutput('+', initialNumber, enterNumber );
-    writeToLog('Add',initialNumber,enterNumber,currentNumber);
+    writeToLog('Add',initialNumber,enterNumber,currentResult);
 }
 
 // This function work for Subtraction
 function subtract() {
     const enterNumber = getUserInputNumber();
-    const initialNumber = currentNumber;
-    currentNumber -= enterNumber;
+    const initialNumber = currentResult;
+    currentResult -= enterNumber;
     createAndWriteOutput('-', initialNumber, enterNumber )
-    writeToLog('Subtract',initialNumber,enterNumber,currentNumber);
+    writeToLog('Subtract',initialNumber,enterNumber,currentResult);
 }
 
 // This function work for Multiply
 function multiply() {
     const enterNumber = getUserInputNumber();
-    const initialNumber = currentNumber;
-    currentNumber *= enterNumber;
+    const initialNumber = currentResult;
+    currentResult *= enterNumber;
     createAndWriteOutput('*', initialNumber, enterNumber )
-    writeToLog('Multiply',initialNumber,enterNumber,currentNumber);
+    writeToLog('Multiply',initialNumber,enterNumber,currentResult);
 }
 
 // This function work for Division
 function divide() {
     const enterNumber = getUserInputNumber();
-    const initialNumber = currentNumber;
-    currentNumber /=  enterNumber;
+    const initialNumber = currentResult;
+    currentResult /=  enterNumber;
     createAndWriteOutput('/', initialNumber, enterNumber )
-    writeToLog('Divide',initialNumber,enterNumber,currentNumber);
+    writeToLog('Divide',initialNumber,enterNumber,currentResult);
 }
 
 // Here we call function for Addition , subtraction, multiplication , division for output
