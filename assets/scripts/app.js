1498e4c985e1b43004c166b99f1d94e407ee04de
= <=>
2022-09-22 12:34:25 +0600

    switch condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,16 +32,34 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
         finalMonsterHealth: monsterHealth,
         finalPlayerHealth: playerHealth,
     };
-    if(ev === LOG_EVENT_PLAYER_ATTACK){
-        logEntry.target = 'MONSTER'
-    }else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
-        logEntry.target = 'MONSTER'
-    }else if (ev === LOG_EVENT_MONSTER_ATTACK){
-        logEntry.target = 'PLAYER'
-    }else if (ev === LOG_EVENT_PLAYER_HEAL){
-       logEntry.target = 'PLAYER'
-    }else if (ev === LOG_EVENT_GAME_OVER){
+    switch (ev) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER'
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry.target = 'MONSTER'
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER'
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER'
+            break;
+        case LOG_EVENT_GAME_OVER:
+            break;
+        default:
+            logEntry = {};
     }
+    // if(ev === LOG_EVENT_PLAYER_ATTACK){
+    //     logEntry.target = 'MONSTER'
+    // }else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+    //     logEntry.target = 'MONSTER'
+    // }else if (ev === LOG_EVENT_MONSTER_ATTACK){
+    //     logEntry.target = 'PLAYER'
+    // }else if (ev === LOG_EVENT_PLAYER_HEAL){
+    //    logEntry.target = 'PLAYER'
+    // }else if (ev === LOG_EVENT_GAME_OVER){
+    // }
     battleLog.push(logEntry);
 }
 
