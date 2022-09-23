9d3a9320a405e91037687cdf56a8dc35e2674a99
= <=>
2022-09-23 08:01:45 +0600

    Indirect function calling

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+function startGame(){
+    console.log("Game is Starting...");
+}
+
+startGameBtn.addEventListener('click',startGame);//Indirect function calling
