b4ddd4de39ddd7c28853a9f1ff71f419a5556646
Yeahyea Sarker <1642cse00575@manarat.ac.bd>
2022-09-24 16:11:07 +0600

    Rock Paper scissers Game make

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-function startGame(){
-    console.log("Game is Starting...");
-}
+let gameIsRunning = false;
 
-startGameBtn.addEventListener('click',startGame);//Indirect function calling
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS} ?`, '').toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS ){
+        alert(`Invalid choice! We Choose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
+
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34){
+        return ROCK;
+    }else if (randomValue < 0.67){
+        return PAPER;
+    }else{
+        return SCISSORS;
+    }
+};
+
+startGameBtn.addEventListener('click', function () {
+    if (gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
