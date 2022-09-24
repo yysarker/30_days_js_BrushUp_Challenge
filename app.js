9fcd1c1d624e3c34d46a35f3fc374473a1c607f6
Yeahyea Sarker <1642cse00575@manarat.ac.bd>
2022-09-24 17:38:08 +0600

    Anonymous function

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
 const startGameBtn = document.getElementById('start-game-btn');
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
-const DEFAULT_USER_CHOICE = ROCK;
 
-let gameIsRunning = false;
-
-const getPlayerChoice = function () {
-    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS} ?`, '').toUpperCase();
-    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS ){
-        alert(`Invalid choice! We Choose ${DEFAULT_USER_CHOICE} for you!`);
-        return DEFAULT_USER_CHOICE;
-    }
-    return selection;
-};
-
-const getComputerChoice = function () {
-    const randomValue = Math.random();
-    if (randomValue < 0.34){
-        return ROCK;
-    }else if (randomValue < 0.67){
-        return PAPER;
-    }else{
-        return SCISSORS;
+const person = {
+    name: 'Max',
+    greet: function greet() {
+        console.log('Hello There!');
     }
 };
-
-startGameBtn.addEventListener('click', function () {
-    if (gameIsRunning){
-        return;
-    }
-    gameIsRunning = true;
-    console.log('Game is starting...');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
-});
+startGameBtn.addEventListener('click', function () {//Anonymous Function
+    console.log('Game is starting..');
+})
